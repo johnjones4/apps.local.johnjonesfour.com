4d9eb2b64a0aed7ecaabbcaef360ebc93ff71b49
John Jones <johnjones4@gmail.com>
2021-07-05 17:37:59 -0400

    back up apps host

--- apps/backup/backup.sh
+++ apps/backup/backup.sh
@@ -4,6 +4,7 @@ export PGPASSWORD="$POSTGRES_PASSWORD"
 
 while :
 do
+  tar zcvf /backup/apps.tar.gz /apps
   while read d; do
     echo "Backing up $d"
     pg_dump "$d" -U $POSTGRES_USER -h $POSTGRES_HOST > /backup/"$d".sql
