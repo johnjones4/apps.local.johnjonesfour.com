8c7e1fdb861811624ca84fd5f486b55f21d3fad2
John Jones <johnjones4@gmail.com>
2021-07-07 19:28:40 -0400

    lpass update

--- apps/lastpass/run.sh
+++ apps/lastpass/run.sh
@@ -2,7 +2,12 @@
 
 while :
 do
-  lpass export --non-interactive > /data/passwords.csv
-  echo $? |& curl -d @- http://jabba:8070/api/jobrun/lastpass
+  lpass export > /data/passwords.csv
+  status=$?
+  if [ "$status" -ne "0" ]; then
+    curl -X POST --data "error: $status" http://jabba:8070/api/jobrun/lastpass
+  else
+    curl -X POST --data "ok: $status" http://jabba:8070/api/jobrun/lastpass
+  fi
   sleep 86400
 done
