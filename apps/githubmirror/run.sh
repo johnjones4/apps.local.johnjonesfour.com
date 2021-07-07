5a4e8284fcc33cb29b5e228821ae8d53dd9a2925
John Jones <johnjones4@gmail.com>
2021-07-07 15:45:02 -0400

    loggng fix

--- apps/githubmirror/run.sh
+++ apps/githubmirror/run.sh
@@ -3,7 +3,7 @@
 while :
 do
   ./mirror.sh &> /tmp/log.txt
-  curl -X POST --data @/tmp/log.txt http://jabba:8070/api/jobrun/github
+  curl -X POST --data-binary @/tmp/log.txt http://jabba:8070/api/jobrun/github
   rm /tmp/log.txt
   sleep 86400
 done
