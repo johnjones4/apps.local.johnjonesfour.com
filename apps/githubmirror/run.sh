f0835a0a74d7e2729d6f32759a7f8e82103b4d76
John Jones <john@johnjonesfour.com>
2021-11-28 13:37:08 -0500

    config fix

--- apps/githubmirror/run.sh
+++ apps/githubmirror/run.sh
@@ -3,7 +3,7 @@
 while :
 do
   ./mirror.sh &> /tmp/log.txt
-  curl -X POST --data-binary @/tmp/log.txt http://jabba-loghandler:8070/api/job/github
+  curl -X POST --data-binary @/tmp/log.txt http://jabba-loghandler:8060/api/job/github
   rm /tmp/log.txt
   sleep "$DELAY_STD"
 done
--- apps/lastpass/run.sh
+++ apps/lastpass/run.sh
@@ -5,9 +5,9 @@ do
   lpass export > /data/passwords.csv
   status=$?
   if [ "$status" -ne "0" ]; then
-    curl -X POST --data "error: $status" http://jabba-loghandler:8070/api/job/lastpass
+    curl -X POST --data "error: $status" http://jabba-loghandler:8060/api/job/lastpass
   else
-    curl -X POST --data "ok: $status" http://jabba-loghandler:8070/api/job/lastpass
+    curl -X POST --data "ok: $status" http://jabba-loghandler:8060/api/job/lastpass
   fi
   sleep "$DELAY_STD"
 done
