c7815d82955b4a4211361b3f0dffda61a4969f4d
John Jones <john@johnjonesfour.com>
2021-11-28 11:57:06 -0500

    jabba redo

--- apps/githubmirror/run.sh
+++ apps/githubmirror/run.sh
@@ -3,7 +3,7 @@
 while :
 do
   ./mirror.sh &> /tmp/log.txt
-  curl -X POST --data-binary @/tmp/log.txt http://jabba:8070/api/jobrun/github
+  curl -X POST --data-binary @/tmp/log.txt http://jabba-loghandler:8060/api/job/github
   rm /tmp/log.txt
   sleep "$DELAY_STD"
 done
