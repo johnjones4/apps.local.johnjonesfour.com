f300bc60781ed127e1c178dd41fb039464599b95
John Jones <john@johnjonesfour.com>
2022-02-22 15:47:48 -0500

    git backup cleanup

--- apps/githubmirror/mirror.sh
+++ apps/githubmirror/mirror.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+create_branch() {
+  branch="${1#origin/}"
+  exists=`git show-ref refs/heads/$branch`
+  if [ ! -n "$exists" ]; then
+    git branch --track "$branch" "$1"
+  fi
+}
+
 backup_repo () {
   repo_path="$DATA_DIR/$1"
   if [ ! -d "$repo_path" ]
@@ -10,6 +18,7 @@ backup_repo () {
   else
     cd "$repo_path"
   fi
+  git branch -r | grep -v '\->' | while read remote; do create_branch $remote; done
   git fetch -f --all
 }
 
